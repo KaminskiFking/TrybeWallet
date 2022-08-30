dc68f46ead948ef4965911b1ebc026b027b8a193
Vinicius Kaminski <vikaminski@hotmail.com>
2022-08-29 23:26:38 -0300

    Excluindo Componente Desnecessario

--- src/components/Services.js
+++
@@ -1,6 +0,0 @@
-export default async function fetchCurrencies() {
-  const requestAPI = await fetch('https://economia.awesomeapi.com.br/json/all');
-  const response = await requestAPI.json();
-  delete response.USDT;
-  return response;
-}
